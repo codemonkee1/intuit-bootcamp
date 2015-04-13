4a60eb4164b96581e375321cde46ed20082be5d1
unknown <slin@MPKL07EF122BC8C.corp.intuit.net>
2015-04-13 15:20:25 -0700

    second commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,25 @@
 module.exports = function(grunt) {
-
-  grunt.initConfig({
-    webServer: {
-      rootFolder: "www",
-      port: 8080
-    }
-  });
-
-  grunt.registerTask("default", "Start web server", function() {
-
-    var
-      http = require("http"),
-      express = require("express"),
-      app = express(),
-      webServerConfig = grunt.config("webServer");
-
-    this.async();
-
-    app.use(express.static(webServerConfig.rootFolder));
-
-    http.createServer(app).listen(webServerConfig.port, function() {
-      grunt.log.writeln("Web server listening on port " + webServerConfig.port);
+    grunt.initConfig({
+	webServer: {
+	    rootFolder: "www",
+	    port: 8080
+	}
     });
 
-  });
+    grunt.registerTask("default", "start a web server", function(){
+
+	var
+	   http = require("http"),
+	   express = require("express"),
+	   app = express(),
+	webServerConfig = grunt.config("webServer");
+
+        this.async();
+
+	app.use(express.static(webServerConfig.rootFolder));
+
+	http.createServer(app).listen(webServerConfig.port, function() {
+	    grunt.log.writeln("web server listening on port: " + webServerConfig.port);
+	});
+    });
 };
